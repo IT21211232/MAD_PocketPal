e65118029dc43d9e7f153f4ee8b55b472d6133a9
Binoosh <wbinoosh@gmail.com>
2023-05-02 18:37:42 +0530

    expense view, expense add, and expense update pages

---
+++ app/src/main/java/com/example/test/UpdateExpenses.kt
@@ -0,0 +1,11 @@
+package com.example.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UpdateExpenses : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_expenses)
+    }
+}
--- app/src/main/java/com/example/test/ViewExpenses.kt
+++ app/src/main/java/com/example/test/ViewExpenses.kt
@@ -5,15 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class Dashboard : AppCompatActivity() {
+class ViewExpenses : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        setContentView(R.layout.activity_view_expenses)
 
-        val viewExpense = findViewById<Button>(R.id.ViewExpenses);
-        viewExpense.setOnClickListener{
+        val addExpense = findViewById<Button>(R.id.addExpenseBtn);
+        addExpense.setOnClickListener{
 
-            val Intent = Intent(this,ViewExpenses::class.java);
+            val Intent = Intent(this,AddExpenses::class.java);
             startActivity(Intent);
         }
     }
